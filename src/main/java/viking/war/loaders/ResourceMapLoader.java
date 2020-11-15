178e894e9873b2d85f2a431ef291eefdb64fe57f
twinevgeni <twinevgeni@gmail.com>
2020-11-15 23:28:21 +0200

    guava dependency removed

--- src/main/java/viking/war/loaders/ResourceMapLoader.java
+++ src/main/java/viking/war/loaders/ResourceMapLoader.java
@@ -1,12 +1,13 @@
 package viking.war.loaders;
 
-import com.google.common.io.Resources;
 import viking.war.objects.IGameMap;
 import viking.war.serializer.MapStringSerializer;
 
-import java.io.PrintStream;
-import java.net.URL;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 /**
  * Map loader from resources
@@ -16,10 +17,28 @@ public class ResourceMapLoader implements IMapLoader {
     private String resName;
     private PrintStream out;
 
-    public ResourceMapLoader(String resName, PrintStream out)
-    {
+    public ResourceMapLoader(String resName, PrintStream out) {
         this.resName = resName;
         this.out = out;
+    }
+
+    /**
+     * Reads given resource file as a string.
+     *
+     * @param fileName path to the resource file
+     * @return the file's contents
+     * @throws IOException if read fails for any reason
+     */
+    static String getResourceFileAsString(String fileName) throws IOException {
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        try (InputStream is = classLoader.getResourceAsStream(fileName)) {
+            if (is == null) return null;
+            try (InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
+                 BufferedReader reader = new BufferedReader(isr)) {
+
+                return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+            }
+        }
     }
 
     /**
@@ -31,15 +50,12 @@ public class ResourceMapLoader implements IMapLoader {
     @Override
     public IGameMap Load() throws Exception {
 
-        if (this.resName.equals(""))
-        {
+        if (this.resName.equals("")) {
             throw new Exception("Карта не выбрана");
         }
 
         this.out.println("Выбрана карта: " + this.resName);
-
-        URL url = Resources.getResource(this.resName);
-        String resMapData = Resources.toString(url, StandardCharsets.UTF_8);
+        String resMapData = getResourceFileAsString(this.resName);
 
         return new MapStringSerializer().deserialize(resMapData);
     }
